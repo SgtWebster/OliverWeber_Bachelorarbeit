c412f3ebae5ba69026f59909af2e7871fe10be2a
Oliver Ulrich Weber <o.weber@mci4me.at>
2026-06-07 21:08:34 +0200

    FixTry 01 - Prisma

--- app/lib/db/prisma.ts
+++ app/lib/db/prisma.ts
@@ -1,7 +1,6 @@
 // prisma.ts
 
-import "dotenv/config";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
